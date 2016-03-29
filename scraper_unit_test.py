aa0955c22197092123d5e549c0a6337e1493dc39
jjenner689 <jjenner689@gmail.com>
2016-03-29 03:40:25 +0100

    Added basic unit testing

--- scraper_unit_test.py
+++ scraper_unit_test.py
@@ -6,22 +6,20 @@ class Scraper(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Firefox()
+        self.COUNTRIES = ['Canada', 'Germany', 'Iceland', 'Pakistan', 'Singapore', 'South Africa']
 
     def test_search_in_python_org(self):
         driver = self.driver
         driver.get('http://international.o2.co.uk/internationaltariffs/calling_abroad_from_uk')
         self.assertIn("O2", driver.title)
         input_form = driver.find_element_by_xpath('//*[@id="countryName"]')
-        for country in COUNTRIES:
-            print_cost(driver, country, input_form)
-        elem = driver.find_element_by_name("q")
-        elem.send_keys("pycon")
-        elem.send_keys(Keys.RETURN)
-        assert "No results found." not in driver.page_source
+        for country in self.COUNTRIES:
+            self.print_cost(driver, country, input_form)
 
-    def print_cost(driver, country, input_form):
+    def print_cost(self, driver, country, input_form):
 
         input_form.send_keys(country, Keys.RETURN)
+        assert "No results found." not in driver.page_source
         tariff_plan = driver.find_element_by_xpath('//*[@id="standardRatesTable"]/tbody/tr[1]/td[2]')
         print("%s: %s" % (country, tariff_plan.get_attribute('innerHTML')))
         input_form.clear()
